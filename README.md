7d99bfc8b1ab84ad2e5ba190eae1fecb4041b68f
Jacob Eugene Lara <Jacob.lara1221@gmail.com>
2021-10-30 16:24:27 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# PyCulator
+PyCulator is a GUI Python program that will create your own calculator.
+
+## Installation
+Use the Package Tkinter 
+> import tkinter as tk 
+## Usage
+>python3 app.py
+
+### Contributing
+Pull Requests are welcome. For major changes, please open an issue first to discuse what you would like to change.
---
+++ app.py
@@ -0,0 +1,77 @@
+from tkinter import *
+
+class Calculator:
+
+
+    def __init__(self, master):
+
+        self.master = master
+
+        master.title("Python Calculator")
+
+        self.equation=Entry(master, width=36, borderwidth=5)
+
+        self.equation.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+        self.createButton()
+    #creates each button with the value we want.
+    def createButton(self):
+        b0 = self.addButton(0)
+        b1 = self.addButton(1)
+        b2 = self.addButton(2)
+        b3 = self.addButton(3)
+        b4 = self.addButton(4)
+        b5 = self.addButton(5)
+        b6 = self.addButton(6)
+        b7 = self.addButton(7)
+        b8 = self.addButton(8)
+        b9 =  self.addButton(9)
+        b_add = self.addButton('+')
+        b_sub = self.addButton('-')
+        b_mult = self.addButton('*')
+        b_div = self.addButton('/')
+        b_clear = self.addButton('c')
+        b_equal = self.addButton('=')
+        #Button list for calculator rows
+        row1=[b7,b8,b9,b_add]
+        row2=[b4,b5,b6,b_sub]
+        row3=[b1,b2,b3,b_mult]
+        row4=[b_clear,b0,b_equal,b_div]
+        #assigns buttons to their location on the gui
+        r=1
+        for row in [row1, row2, row3, row4]:
+            c=0
+            for buttn in row:
+                buttn.grid(row=r, column=c, columnspan=1)
+                c+=1
+            r+=1
+
+    def addButton(self,value):
+        #makes the button useable
+        return Button(self.master, text=value, width=9, command = lambda: self.clickButton(str(value)))
+
+    def clickButton(self,value):
+        #User input
+        current_equation=str(self.equation.get())
+        #if the user clicked "c" then clear the line.
+        if value =='c':
+            self.equation.delete(-1, END)
+        #if user clickes = then compute and display
+        elif value == '=':
+            answer = str(eval(current_equation))
+            self.equation.delete(-1, END)
+            self.equation.insert(0, answer)
+        #if user clicked any other butten add it to the line.
+        else:
+            self.equation.delete(0, END)
+            self.equation.insert(-1, current_equation+value)
+
+
+    """Execution"""
+if __name__ =='__main__':
+
+    root = Tk()
+
+    my_gui = Calculator(root)
+
+    root.mainloop()
